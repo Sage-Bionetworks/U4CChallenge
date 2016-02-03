4297272b7f1c2b5ede4670a5eba2fbdfa1edd8df
kdaily <kdaily@users.noreply.github.com>
2016-02-02 17:00:55 -0800

    added code to archive

---
+++ R/archiveProject.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env r
+
+if (is.null(argv) | length(argv)<1) {
+  cat("Usage: archiveProject.R submissionId\n")
+  q()
+}
+
+library(synapseClient)
+library(synapseCopyProject)
+
+synapseLogin()
+
+# Submission
+submission <- synGetSubmission(argv[1])
+
+# Old project
+oldPId <- submission$entityId
+oldP <- synGet(oldPId)
+
+# Crawl it
+G <- crawlSynapseObject(oldPId)
+
+# New project will be copy of the old one
+newP <- synStore(Project(name=paste("Archived ", as.numeric(Sys.time()), submission$id, oldP@properties$id, oldP@properties$name)))
+
+# Perform a copy
+pC <- copyProject(newP@properties$id, G, topId=newP@properties$id)
+
+# Create entity mapping to update wikis
+entityMap <- pC$newid
+names(entityMap) <- G$id
+
+# copy Wikis
+res <- lapply(seq_along(entityMap),
+              function(i) tryCatch(copyWiki(names(entityMap[i]), entityMap[i],
+                                            entityMap = entityMap),
+                                   error=function(e) NULL))
