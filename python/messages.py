6a98347cef75ffe367c4f3b84f59c939702bf5ed
Kenneth Daily <kmdaily@gmail.com>
2016-02-24 22:40:47 +0000

    updated messages

--- python/messages.py
+++ python/messages.py
@@ -50,7 +50,7 @@ Project ID: <b>{project_id}</b>
 <p>If you have questions, please ask on the forums at {support_forum_url}.</p>
 
 <p>Sincerely,<br>
-the scoring script</p>
+The U4C Committee Validation Script</p>
 """
 
 validation_passed_subject_template = "Submission received to {queue_name}"
@@ -76,7 +76,7 @@ Project ID: <b>{project_id}</b>
 instructions which can be found at {challenge_instructions_url}.</p>
 
 <p>Sincerely,<br>
-the scoring script</p>
+The U4C Committee Validation Script</p>
 """
 
 scoring_succeeded_subject_template = "Scored submission to {queue_name}"
