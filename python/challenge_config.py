8d125390c22c3b1c24b781219548476742253cf5
Kenneth Daily <kmdaily@gmail.com>
2016-02-24 21:57:08 +0000

    add stuff specific for this challenge

--- python/challenge_config.py
+++ python/challenge_config.py
@@ -6,19 +6,19 @@
 
 import validation_helpers
 import synapseclient
-syn = synapseclient.login()
+syn = synapseclient.login(silent=True)
 
 ## A Synapse project will hold the assetts for your challenge. Put its
 ## synapse ID here, for example
 ## CHALLENGE_SYN_ID = "syn1234567"
-CHALLENGE_SYN_ID = "syn5591679"
+CHALLENGE_SYN_ID = "syn3157598"
 
 ## Name of your challenge, defaults to the name of the challenge's project
-CHALLENGE_NAME = "Example Synapse Challenge"
+CHALLENGE_NAME = "NCI Up For A Challenge"
 
 ## Synapse user IDs of the challenge admins who will be notified by email
 ## about errors in the scoring script
-ADMIN_USER_IDS = ["3323072"]
+ADMIN_USER_IDS = ["3323072", "2224171", "3323809"]
 
 ## Each question in your challenge should have an evaluation queue through
 ## which participants can submit their predictions or models. The queues
@@ -33,7 +33,21 @@ ADMIN_USER_IDS = ["3323072"]
 ## Configuring them here as a list will save a round-trip to the server
 ## every time the script starts.
 evaluation_queues = [
-    {'status': u'OPEN', 'contentSource': u'syn5591679', 'submissionInstructionsMessage': u'To submit to the XYZ Challenge, send a tab-delimited file as described here: https://...', u'createdOn': u'2016-01-15T22:38:09.697Z', 'submissionReceiptMessage': u'Your submission has been received. For further information, consult the leader board at https://...', u'etag': u'a9e827f7-ca98-4157-bfb8-72bbb1617872', u'ownerId': u'3323072', u'id': u'5591680', 'name': u'Example Synapse Challenge d9372db4-986d-4870-8277-70c1dac57b0e'}]
+{u'contentSource': u'syn3157598',
+ u'createdOn': u'2015-06-15T17:34:55.090Z',
+ u'description': u'The goal of this Challenge is to use innovative approaches to identify novel pathways - including new genes or combinations of genes, genetic variants, or sets of genomic features - involved in breast cancer susceptibility in order to generate new biological hypotheses.',
+ u'etag': u'fe52c239-58a0-4d79-b0f4-3348ac50e33f',
+ u'id': u'4484862',
+ u'name': u'NCI Up For a Challenge (U4C)',
+ u'ownerId': u'3323072',
+ u'quota': {u'firstRoundStart': u'2016-02-09T16:47:20.000Z',
+  u'roundDurationMillis': 1,
+  u'submissionLimit': 100},
+ u'status': u'OPEN',
+ u'submissionInstructionsMessage': u'In order to submit a Challenge Entry, your team will create a Synapse Project which will address the evaluation criteria for the Challenge, along with references to source code and a wiki documenting your methods.',
+ u'submissionReceiptMessage': u'Congratulations! Your submission has been received and will be evaluated by the Challenge organizing committee.'}
+    # {'status': u'OPEN', 'contentSource': u'syn5591679', 'submissionInstructionsMessage': u'To submit to the XYZ Challenge, send a tab-delimited file as described here: https://...', u'createdOn': u'2016-01-15T22:38:09.697Z', 'submissionReceiptMessage': u'Your submission has been received. For further information, consult the leader board at https://...', u'etag': u'a9e827f7-ca98-4157-bfb8-72bbb1617872', u'ownerId': u'3323072', u'id': u'5591680', 'name': u'Example Synapse Challenge d9372db4-986d-4870-8277-70c1dac57b0e'}
+]
 evaluation_queue_by_id = {q['id']:q for q in evaluation_queues}
 
 ## define the default set of columns that will make up the leaderboard
